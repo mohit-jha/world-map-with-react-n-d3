537b2980fb49b4ef28bf93f8d49ee1561f169bad
Mohit_jha <mohitjha1511@gmail.com>
2020-09-04 18:29:15 +0530

    ToDo App in React

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './Form.css'
 import Todo from './Todo'
-ReactDOM.render(<Todo /> ,document.getElementById('bgevents'));
+ReactDOM.render(<Todo /> ,document.getElementById('todo'));
 
